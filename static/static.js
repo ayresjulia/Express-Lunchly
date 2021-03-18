13a54dceaddad0dbdfc2a811237c8fb3b195e409
Julia Ayres <j.bobrineva@gmail.com>
2021-03-18 17:45:28 -0400

    added static js file

--- static/static.js
+++ static/static.js
@@ -1,10 +1,25 @@
-// let first = $(".first-name");
-// let last = $(".last-name");
+// capitalize first letter of search inputs
 
-$(".search-name").on("submit", () => {
-	alert("works");
-	// if (first && last) {
-	// 	first.css("backgroundColor", "grey");
-	// 	last.css("backgroundColor", "grey");
-	// }
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// if customer name exists in the list, set background to green
+
+$(".search-name").on("submit", function (e) {
+	e.preventDefault();
+	$(".cust-li").css("background-color", "white");
+	let first = $(".first-name").val().toLowerCase();
+
+	let last = $(".last-name").val().toLowerCase();
+
+	let full = capitalizeFirstLetter(first) + " " + capitalizeFirstLetter(last);
+	let list = $(".customer-list").html();
+	$(document).ready(function () {
+		if (!list.includes(full)) {
+			alert(`Customer ${full} not found, please try again.`);
+		} else {
+			$(`li:contains(${full})`).css("background-color", "#0D5727");
+		}
+	});
 });
