f3f13efda3ab7c4fe50745318817c33ffeafff25
Julia Ayres <j.bobrineva@gmail.com>
2021-03-18 15:19:47 -0400

    initial commit

---
+++ static/static.js
@@ -0,0 +1,10 @@
+// let first = $(".first-name");
+// let last = $(".last-name");
+
+$(".search-name").on("submit", () => {
+	alert("works");
+	// if (first && last) {
+	// 	first.css("backgroundColor", "grey");
+	// 	last.css("backgroundColor", "grey");
+	// }
+});
